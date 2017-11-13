d2f36b6acb7bfd8e66ae2bda86186a8538d035ea
twankim <twankim@utexas.edu>
2017-11-13 16:17:14 -0600

    Changed gitignore

---
+++ _init_paths.py
@@ -0,0 +1,26 @@
+# Modifications copyright 2017 UT Austin/Taewan Kim
+
+import os
+import sys
+
+def add_path(path):
+    if path not in sys.path:
+        sys.path.append(path)
+
+this_path = os.path.dirname(__file__)
+
+tf_research_path = os.path.join(this_path,'..','models','research')
+
+det_path = os.path.join(tf_research_path,'object_detection')
+slim_path = os.path.join(tf_research_path,'slim')
+
+if not os.path.exists(tf_research_path):
+    raise ValueError('You must download tensorflow research models'
+                     'https://github.com/tensorflow/models/tree/master/research')
+if not os.path.exists(det_path):
+    raise ValueError('You must download tensorflow research object detectino API')
+if not os.path.exists(slim_path):
+    raise ValueError('You must download tensorflow research slim API')
+
+add_path(slim_path)
+add_path(det_path)
