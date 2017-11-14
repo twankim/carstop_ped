c8e4ceab8edd15e6f4c549437499c5467647f893
twankim <twankim@utexas.edu>
2017-11-14 11:30:18 -0600

    Minor update

--- _init_paths.py
+++ _init_paths.py
@@ -11,16 +11,16 @@ this_path = os.path.dirname(__file__)
 
 tf_research_path = os.path.join(this_path,'..','models','research')
 
-det_path = os.path.join(tf_research_path,'object_detection')
+# det_path = os.path.join(tf_research_path,'object_detection')
 slim_path = os.path.join(tf_research_path,'slim')
 
 if not os.path.exists(tf_research_path):
     raise ValueError('You must download tensorflow research models'
                      'https://github.com/tensorflow/models/tree/master/research')
-if not os.path.exists(det_path):
-    raise ValueError('You must download tensorflow research object detectino API')
-if not os.path.exists(slim_path):
-    raise ValueError('You must download tensorflow research slim API')
+# if not os.path.exists(det_path):
+#     raise ValueError('You must download tensorflow research object detectino API')
+# if not os.path.exists(slim_path):
+#     raise ValueError('You must download tensorflow research slim API')
 
-add_path(slim_path)
-add_path(det_path)
+add_path(tf_research_path)
+# add_path(slim_path)
--- ped_detect.py
+++ ped_detect.py
@@ -22,11 +22,24 @@ import os
 import sys
 import tensorflow as tf
 
-if tf.__version__ != '1.4.0':
-    raise ImportError('Please upgrade your tensorflow installation to v1.4.0!')
+tf.app.flags.DEFINE_string(
+    'model', 'faster_rcnn_nas_coco_2017_11_08',
+    'model name to use for object detection')
 
-sys.path.append('')
+# tf.app.flags.DEFINE_integer(
+#     'is_rand', True, 'Turn on random decalibration')
+
+# tf.app.flags.DEFINE_boolean(
+#     'is_crop', True, 'Eval image size')
+
+FLAGS = tf.app.flags.FLAGS
+
 def main(_):
+    if tf.__version__ != '1.4.0':
+        raise ImportError('Please upgrade your tensorflow installation to v1.4.0!')
+    
+    # if tf.gfile.IsDirectory(FLAGS.model):
+    model_name = os.path.join('pretrained',FLAGS.model,'frozen_inference_graph.pb')
 
 
 if __name__ == '__main__':
