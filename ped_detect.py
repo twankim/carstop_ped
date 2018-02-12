0f1a8b866372311360c5d42a8deebe41ef628971
twankim <twankim@utexas.edu>
2018-02-12 15:58:55 -0600

    Image/LIDAR/Label Generation

--- ped_detect.py
+++ ped_detect.py
@@ -76,6 +76,9 @@ tf.app.flags.DEFINE_boolean(
 
 FLAGS = tf.app.flags.FLAGS
 
+_FILE_VIDEO = 'cam.mkv'
+_FILE_LIDAR = 'lidar.dat'
+_FILE_TIMES = 'timestamps.txt'
 
 def get_valid_label_list(data_pre):
     if data_pre == 'coco':
@@ -126,12 +129,12 @@ def main(_):
         path_out = os.path.join(FLAGS.dout,fname)
     else:
         path_out = FLAGS.input
-    if os.path.exists(os.path.join(FLAGS.input,'cam.mkv')):
+    if os.path.exists(os.path.join(FLAGS.input,_FILE_VIDEO)):
         path_image = os.path.join(path_out,'image')
         gen_image = True
         if not os.path.exists(path_image):
             os.makedirs(path_image)
-    if os.path.exists(os.path.join(FLAGS.input,'lidar.dat')):
+    if os.path.exists(os.path.join(FLAGS.input,_FILE_LIDAR)):
         path_lidar = os.path.join(path_out,'lidar')
         gen_lidar = True
         if not os.path.exists(path_lidar):
@@ -141,12 +144,12 @@ def main(_):
         os.makedirs(path_label)
 
     # Load time stamps
-    # Text file 'timestamps.txt' is located with 'cam.mkv' and 'lidar.dat'
-    path_time = os.path.join(FLAGS.input,'timestamps.txt')
+    path_time = os.path.join(FLAGS.input,_FILE_TIMES)
     time_stamps = []
     with open(path_time,'r') as f_time:
         for line in f_time:
             time_stamps.append(convert_timestamps(line))
+    n_frames = [] # Number of frames to be saved
 
     # Load label map (pretrained model)
     label_map = label_map_util.load_labelmap(FLAGS.label)
@@ -178,8 +181,9 @@ def main(_):
                 tf.import_graph_def(od_graph_def, name='')
 
         # Read video and do object detection
-        input_video = os.path.join(FLAGS.input,'cam.mkv')
+        input_video = os.path.join(FLAGS.input,_FILE_VIDEO)
         i_save = 0 # image name indexing
+        sum_frames = 0 # Current total number of frames
         r_fps = FLAGS.fps_in/FLAGS.fps_out # ratio of input/output fps
         assert (FLAGS.fps_in % FLAGS.fps_out) == 0,\
             "{} must be divisible by the Target FPS".format(FLAGS.fps_in)
@@ -205,14 +209,14 @@ def main(_):
                                     'num_detections:0')
         
                 # Save frames only from the selected time frames
-                for i_test,time_stamp in enumerate(time_stamps):
+                for time_stamp in time_stamps:
                     videogen = vreader(input_video,
                                        num_frames=int(time_stamp[2]*FLAGS.fps_in),
                                        inputdict={'-r':str(FLAGS.fps_in)},
                                        outputdict={'-r':str(FLAGS.fps_in),
                                                    '-ss':time_stamp[0],
                                                    '-t':time_stamp[1]})
-                    print('-Time stamp start: {}, Duration: {} (secs)'.format(
+                    print('<Image&Label> Time stamp start: {}, Duration: {} (secs)'.format(
                                     time_stamp[0],
                                     time_stamp[2]))
                     i_frame = 0
@@ -278,6 +282,11 @@ def main(_):
                                 plt.imshow(image_np)
                             i_save +=1
                         i_frame += 1
+                    if len(n_frames) == 0:
+                        n_frames.append(i_save)
+                    else:
+                        n_frames.append(i_save-sum_frames)
+                    sum_frames = i_save
         
         # Save output video with bounding boxes
         if FLAGS.is_vout:
@@ -290,5 +299,25 @@ def main(_):
     # Read LIDAR data and generate point cloud files per frame
     if gen_lidar:
         print('... Generating LIDAR point clouds per frame.')
+        input_lidar = os.path.join(FLAGS.input,_FILE_LIDAR)
+        i_save = 0 # lidar name indexing
+
+        for idx_t, time_stamp in enumerate(time_stamps):
+            print('<LIDAR> Time stamp start: {}, Duration: {} (secs)'.format(
+                                    time_stamp[0],
+                                    time_stamp[2]))
+            # Read LIDAR file
+            start_time = tstamp2sec(time_stamp[0])
+            lidar_out = lread(input_lidar,start_time,start_time+time_stamp[2])
+            if len(n_frames)>0:
+                n_frame = n_frames[idx_t]
+                assert n_frame >= len(lidar_out), \
+                    "* Number of frames in video is larger than the ones in LIDAR!"
+                lidar_out = lidar_out[:n_frame] # Match frame numbers with images
+
+            for ldata in lidar_out:
+                ldata[1].tofile(os.path.join(path_lidar,'{:06d}.bin'.format(i_save)))
+                i_save += 1
+
 if __name__ == '__main__':
     tf.app.run()
