89a50fe379f99db7fc6b3d2c7a3a077124f880c2
twankim <twankim@utexas.edu>
2018-04-25 16:26:19 -0500

    Testing inception

---
+++ scripts/gen_video_accord_ssd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+
+DATA_PRE=coco
+FILE_MODEL=pretrained/ssd_inception_v2_coco_2017_11_17/frozen_inference_graph.pb
+LABEL_MAP=../models/research/object_detection/data/mscoco_label_map.pbtxt
+NUM_CLASS=9
+FPS_IN=30
+FPS_OUT=30
+
+python cstopp_gen_video.py \
+        --data_pre=${DATA_PRE} \
+        --model=${FILE_MODEL} \
+        --label=${LABEL_MAP} \
+        --num_classes=${NUM_CLASS} \
+        --input=/data/cstopp/accord_040118 \
+        --fps_in=${FPS_IN} \
+        --fps_out=${FPS_OUT} \
+        --is_rotate=True
